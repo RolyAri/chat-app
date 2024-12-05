f62598046d1070e08df0d18276de41a8d0775680
Roly Ari <109258322+RolyAri@users.noreply.github.com>
2024-12-05 00:30:17 -0500

    Revert "update server"
    This reverts commit eae2055e0a0a52354b8768dddb917403aa7afd9d.

--- chat-app-server/indexBefore.js
+++ chat-app-server/indexBefore.js
@@ -4,6 +4,65 @@ const typeDefs = require("./typedefs");
 const resolvers = require("./resolvers");
 const jwt = require("jsonwebtoken");
 
+/* const users = [
+    {
+      id: "sdaddsada",
+      firstName: "mukesh",
+      lastName: "kumar",
+      email: "mukesh@mukeshkumar.com",
+      password: "12345"
+    },
+    {
+      id: "wfwrwr",
+      firstName: "suresh",
+      lastName: "sharma",
+      email: "suresh@sureshsharma.com",
+      password: "12346"
+    }
+  ];
+
+  const todos = [
+    {
+      title: "buy book",
+      by: "sdaddsada"
+    },
+    {
+      title: "write code",
+      by: "sdaddsada"
+    },
+    {
+      title: "record video",
+      by: "wfwrwr"
+    }
+  ]
+  
+ */
+
+/* const resolvers = {
+    Query: {
+      users: () => users, 
+      user: (_, {id}, {userLoggedIn}) =>{
+        if(!userLoggedIn) throw new Error("no estas logeado")
+        console.log(userLoggedIn)
+        return users.find(item => item.id == id)
+      } 
+    },
+    User: {
+        todos:(parent) => {
+            console.log("entrando bd")
+            return todos.filter(todo=>todo.by == parent.id)
+        }
+    },
+    Mutation: {
+        createUser: (_,{userNew}) => {
+            const newUser = {id: crypto.randomUUID(),
+                ...userNew}
+            users.push(newUser)
+            return newUser
+
+        }
+    }
+  }; */
 const server = new ApolloServer({
   typeDefs,
   resolvers,
